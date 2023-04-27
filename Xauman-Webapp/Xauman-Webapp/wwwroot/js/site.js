48cb583a295be01174f8d238668a06b90fedd380
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-04-28 00:55:29 +0300

    landing page header section added

--- Xauman-Webapp/Xauman-Webapp/wwwroot/js/site.js
+++ Xauman-Webapp/Xauman-Webapp/wwwroot/js/site.js
@@ -173,6 +173,18 @@ var Xauman = {
     }
 };
 
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("nav-bg").style.top = "0";
+    } else {
+        document.getElementById("nav-bg").style.top = "-50px";
+    }
+}
+
 $(document).ready(function () {
     Xauman.init();
 });
