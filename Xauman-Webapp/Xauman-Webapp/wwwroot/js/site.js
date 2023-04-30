82704a72e78945ff261e11cffb5d92994d0a887d
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-04-30 10:23:20 +0300

    Added swiper script initializer

--- Xauman-Webapp/Xauman-Webapp/wwwroot/js/site.js
+++ Xauman-Webapp/Xauman-Webapp/wwwroot/js/site.js
@@ -173,23 +173,23 @@ var Xauman = {
     }
 };
 
-//// Swiper Initialization
-//var swiper = new Swiper(".mySwiper", {
-//    effect: "coverflow",
-//    grabCursor: true,
-//    centeredSlides: true,
-//    slidesPerView: "auto",
-//    coverflowEffect: {
-//        rotate: 50,
-//        stretch: 0,
-//        depth: 100,
-//        modifier: 1,
-//        slideShadows: true,
-//    },
-//    pagination: {
-//        el: ".swiper-pagination",
-//    },
-//});
+// Swiper Initialization
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
 
 $(document).ready(function () {
     Xauman.init();
